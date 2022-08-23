565c895c1e303548600e3ba1d970b67d7dd19352
malveslobo <malveslobo@click2buy.com>
2022-08-23 15:00:58 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # hadoop_metrics_exporter
 Hadoop metrics exporter for prometheus
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://click2buy.com/wp-content/uploads/2021/07/Logo-Click2buy.png)
+
+Actually without Ambari we don't have possibility to get hadoop metrics.
+This project set sevral metrics for hadoop application like hbase, yarn , hive
+
+We use ruby on rails and prometheus gem.
+
+### Built With
+
+
+
+* [![Ruby][ruby]]Ruby-url]
+* [![Docker][Docker]][Docker-url]
+
+
+## Getting Started
+
+We can use docker or not (dockzer is recommanded for better compatibilty)
+
+In any case , you must upload 2 conf files
+
+#### metrics.yml ####
+
+list of all metrics available, see metrics.yml.example
+
+you can add metrics 
+
+choose application
+
+  ```yaml
+  hbase:
+  SystemCpuLoad:
+    desc: 'cpu hive server'
+    metricType: 'gauge
+  ```
+
+#### exporter.yml ####
+
+
+Conf for access to api metrics 
+
+  ```yaml
+    exporter:
+    bind: 0.0.0.0
+    port: 7629
+    interval: 60
+    verbose: true
+    components:
+    hbase:
+        url:        http://hbase/jmx
+        basic_auth:
+        username:   hbase
+        password:   hbase
+    yarn:
+        url:      http://yarn/ws/v1/cluster/metrics
+        basic_auth:
+        username:   yarn
+        password:   yarn
+    namenode:
+        url:      http://namenode.com/jmx
+        basic_auth:
+        username:   namenode
+        password:   namenode
+  ```
+
+  bind: address where process listen
+  port: port listen
+  interval: interval in second between each api call
+  basic_auth: in case of a minimum of security
+
+### with docker
+
+
+* create conf and metrics directories
+  ```sh
+  mkdir conf
+  mkdir metrics
+  ```
+
+* paste metrics/metrics.conf inside metrics directory
+
+  ```sh
+touch metrics/metrics.yml
+  ```
+
+* same for exporter.yml conf
+
+  ```sh
+touch conf/exporter.yml
+  ```
+
+  download docker-compose exemple and run 
+
+    ```sh
+docker-compose -f docker-compose.yml up -d
+  ```
--- exporter.rb
+++ exporter.rb
@@ -10,14 +10,21 @@ require 'json'
 require 'yaml'
 
 
+
+#METHODS
+def underscore name
+  name.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").downcase
+end
+
 #VARIABLES
 configuration   = YAML.load_file('conf/exporter.yml')
 bind            = configuration['exporter']['bind'] || '0.0.0.0'
-port            = configuration['exporter']['port'] || 12346
-interval        = configuration['exporter']['bind'] || 60
+port            = configuration['exporter']['port'] || 7629
+interval        = configuration['exporter']['interval'] || 60
 verbose         = configuration['exporter']['verbose'] || false
 components      = configuration['components'] || []
-
+default_metrics =  YAML.load_file('metrics/metrics.yml')
+values          = {}
 # bind is the address, on which the webserver will listen
 # port is the port that will provide the /metrics route
 server = PrometheusExporter::Server::WebServer.new bind: bind , port: port , verbose: verbose
@@ -25,15 +32,56 @@ server.start
 #Instance a client and metrics to collect 
 client =  PrometheusExporter::LocalClient.new(collector: server.collector)
 
+# Filter and keep metrics to fill
+metrics = default_metrics.select {|k, v| components.keys.include? k}
+components.each do |component|
+  if component.last['include']
+    metrics[component.first].select!{|k, v| component.last['include'].include? k}
+  end
+end
 
+#Instanciate metrics
+metrics.each do |component|
+  component_name  = component.first
+  component.last.each do |metric| 
+    metric_name     = metric.first
+    formatted_key   = underscore(metric_name)
+    values["#{component_name}_#{formatted_key}"]  = client.register( metrics[component_name][metric_name]['metricType'].to_sym, "#{component_name}_#{formatted_key}", metrics[component_name][metric_name]['desc']) 
+  end
+end
+
+#Set metrics
 while true
 
-    components.each do |component|
+  components.each do |component|
+    name = component.first
+    uri = URI(component.last['url'])
 
-      component
+    Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https', 
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+      request = Net::HTTP::Get.new uri.request_uri
+      
+      if component.last['basic_auth']
+        request.basic_auth component.last['basic_auth']['username'], component.last['basic_auth']['password']
+      end
 
+      response      = http.request request # Net::HTTPResponse object
+      result        = JSON.parse(response.body)
+      if name == 'yarn'
+        result['clusterMetrics'].select{|k, v| metrics[name].keys.include? k}.each do |metric|
+          formatted_key          = underscore( metric.first )
+          values["#{name}_#{formatted_key}"].observe( metric.last )
+        end
+      else 
+        result["beans"].map{ |obj| obj.select{ |k,v| metrics[name].keys.include? k}}.reject(&:empty?).reduce( Hash.new, :merge ).each do |metric|
+          formatted_key          = underscore( metric.first )
+          values["#{name}_#{formatted_key}"].observe( metric.last )
+        end
+      end
     end
-
-    sleep interval.to_i
+  end
+  
+  sleep interval
 
 end
